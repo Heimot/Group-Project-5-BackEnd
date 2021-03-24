d5350b446da7a19c7c179644e2f6b68d8486b9e8
Santeri Korpinen <n0koja00@students.oamk.fi>
2021-03-24 11:08:15 +0200

    id, product, category, subcategory. functions, headers

---
+++ inc/functions.php
@@ -0,0 +1,14 @@
+<?php 
+// Yhteys tietokantaan funktiona
+function slDB() {
+   $db = new PDO('mysql:host=localhost;dbname=digifemma;charset=utf8', 'root', '');
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   return $db;
+}
+// Virheilmoitus tiedon haussa
+function returnError(PDOException $pdoex) {
+   echo header('http/1.1 500 Internal Server Error');
+   $error = array('error' => $pdoex->getMessage());
+   echo json_encode($error);
+   exit;
+}
