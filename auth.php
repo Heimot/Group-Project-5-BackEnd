41fb88baa48791b5c4960b729dbb8618e8f27079
HeliotropicAfterglow <HeliotropicAfterglow@users.noreply.github.com>
2021-04-03 03:37:39 +0300

    Authorization

---
+++ auth.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'inc/headers.php';
+require_once 'inc/functions.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$email = filter_var($input->email, FILTER_SANITIZE_STRING);
+$password = filter_var($input->password, FILTER_SANITIZE_STRING);
+
+try {
+    $db = openDb();
+
+    $query = "select firstname from customers where email=:email and password =:password";
+    $query = $db->prepare($query);
+    $query->bindParam('email', $email, PDO::PARAM_STR);
+    $query->bindValue('password', $password, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo header('HTTP/1.1 200 OK');
+    echo json_encode($results);
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
+?>
