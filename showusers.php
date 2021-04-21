293ecc3671019d1634b6413e2f35b708b16e14d6
HeliotropicAfterglow <HeliotropicAfterglow@users.noreply.github.com>
2021-04-21 17:14:33 +0300

    user table

---
+++ showusers.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'inc/headers.php';
+require_once 'inc/functions.php';
+
+try {
+   $db = slDB();
+   $sql = "select id, email, firstname, lastname, address, postalcode, city, phone from customers"; 
+   $query = $db->query($sql);
+   $results = $query->fetchAll(PDO::FETCH_ASSOC);   
+   echo header ('HTTP/1.1 200 OK');
+   echo json_encode($results);
+}
+
+catch (PDOException $pdoex) {
+   returnError($pdoex);
+}
