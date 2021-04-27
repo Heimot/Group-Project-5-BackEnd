75720242b5341330c7339d50928d37688b14e179
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-04-27 16:11:14 +0300

    Fixed some stuff

--- id.php
+++ id.php
@@ -4,18 +4,11 @@ require_once 'inc/headers.php';
 require_once 'inc/functions.php';
 
 $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-$img = filter_var($_GET['img'], FILTER_SANITIZE_NUMBER_INT);
-
-if($img > 0) {
-   $method = " group by product.id";
-} else {
-   $method = "";
-}
 
 // Noudetaan tietokannasta halutut tiedot
 try {
    $db = slDB();
-   $sql = 'select product.id, product.name, product.price, product.description, product.weight, product.stock, productpictures.picture from product left join productpictures on product.id = productpictures.productId WHERE product.id='. $id;
+   $sql = 'select product.id, product.name, product.price, product.description, product.weight, product.stock, productpictures.picture from product left join productpictures on product.id = productpictures.productId WHERE product.id = '. $id;
    $query = $db->query($sql);
    $results = $query->fetchAll(PDO::FETCH_ASSOC);
    echo header ('http1.1 200 OK');
