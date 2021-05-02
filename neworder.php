7c7be4342e8fcf715d3ab58a1dd8b61254403047
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-05-02 22:37:21 +0300

    Added ordercomment

--- neworder.php
+++ neworder.php
@@ -4,7 +4,7 @@ require_once 'inc/headers.php';
 
 $input = json_decode(file_get_contents('php://input'));
 
-$registered = false;
+//$registered = false;
 
 $customerid = filter_var($input->customerid, FILTER_SANITIZE_NUMBER_INT);
 $price = filter_var($input->price, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND);
@@ -16,18 +16,19 @@ $address = filter_var($input->address, FILTER_SANITIZE_STRING);
 $postalcode = filter_var($input->postalcode, FILTER_SANITIZE_STRING);
 $city = filter_var($input->city, FILTER_SANITIZE_STRING);
 $phone = filter_var($input->phone, FILTER_SANITIZE_STRING);
+$orderComment = filter_var($input->orderComment, FILTER_SANITIZE_STRING);
 $cart = $input->cart;
 
 try {
     $db = slDB();
-    $query = $db->prepare('insert into orders(customerid,price,shipping,email,firstname,lastname,address,postalcode,city,phone) values (:customerid,:price,:shipping,:email,:firstname,:lastname,:address,:postalcode,:city,:phone)');
-    if ($registered) {
+    $query = $db->prepare('insert into orders(customerid,price,shipping,email,firstname,lastname,address,postalcode,city,phone,orderComment) values (:customerid,:price,:shipping,:email,:firstname,:lastname,:address,:postalcode,:city,:phone,:orderComment)');
+    /*if ($registered) {
         $query->bindValue(':customerid',$customerid,PDO::PARAM_STR);
     } else {
         $query->bindValue(':customerid',null,PDO::PARAM_STR);
-    }
+    }*/
     
-    
+    $query->bindValue(':customerid',$customerid,PDO::PARAM_INT);
     $query->bindValue(':price',$price,PDO::PARAM_STR);
     $query->bindValue(':shipping',$shipping,PDO::PARAM_STR);    
     $query->bindValue(':email',$email,PDO::PARAM_STR);
@@ -37,6 +38,7 @@ try {
     $query->bindValue(':postalcode',$postalcode,PDO::PARAM_STR);
     $query->bindValue(':city',$city,PDO::PARAM_STR);
     $query->bindValue(':phone',$phone,PDO::PARAM_STR);
+    $query->bindValue(':orderComment',$orderComment,PDO::PARAM_STR);
     $query->execute();
 
     $lastinsertid = $db->lastInsertId();
